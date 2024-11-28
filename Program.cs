37e2177b5d46daf8bcdb10acb6bf3e93d1867591
ktv11 <ktv11@KOTEIIZE>
2024-11-28 23:48:27 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        bool m = true;
+        while (m)
+        {
+            {
+                Console.WriteLine("Введите числа:");
+                int n = int.Parse(Console.ReadLine());
+                if (n <= 0)
+                {
+                    m = false;
+                }
+                while (n > 0)
+                {  
+                    int count = 0;
+                    int res = 0;
+                    int b = n;
+                    while (b > 0)
+                    {
+                        if ((b % 10) % 2 != 0)
+                        {
+                            b = b / 10;
+                        }
+                        else
+                        {
+                            res = res * 10 + b % 10;
+                            b = b / 10;
+                            count++;
+                        }
+                    }
+                    
+                    if (count == 0)
+                    {
+                        Console.WriteLine("Четных цифр нет");
+                        break;
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine(res);
+                        break;
+                    }
+                    
+                }
+            }
+        }
+    }  
+}
